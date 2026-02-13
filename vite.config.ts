39c1d2a26609c22709f85a43ffbe757a73029596
jekeytimoy29 <mothj20@gmail.com>
2026-02-14 05:37:34 +0800

    Fixed build and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio-v3-gen-ai-web-portfolio/', // 👈 IMPORTANT
 })
